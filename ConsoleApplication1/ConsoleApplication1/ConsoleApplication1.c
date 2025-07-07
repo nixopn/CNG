f031346a6db4ab815d2ee79edd702d246cd581ab
nixopn <eisnersandy2@gmail.com>
2025-07-07 11:07:32 +0300

    Added working create hash function in consoleapplication1 B

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.c
@@ -9,24 +9,75 @@
 
 #define NT_SUCCESS(Status)          (((NTSTATUS)(Status)) >= 0)
 
+
+
+void CrHash(NTSTATUS *status, BCRYPT_ALG_HANDLE *hHashAlg, BCRYPT_HASH_HANDLE *hHash, DWORD cbDigest, DWORD cbResult, PBYTE msg, PBYTE pbHash) {
+    pbHash = (PBYTE)HeapAlloc(GetProcessHeap(), 0, cbDigest);
+    status = BCryptOpenAlgorithmProvider(&hHashAlg, BCRYPT_SHA1_ALGORITHM, NULL, 0);
+    if (!NT_SUCCESS(status)) {
+        printf("Error1 0x%x \n", status);
+        goto cleanupCrHash;
+    }
+    status = BCryptGetProperty(hHashAlg, BCRYPT_HASH_LENGTH, (PBYTE)&cbDigest, sizeof(cbDigest), &cbResult, 0);
+    if (!NT_SUCCESS(status)) {
+        printf("Error2 0x%x \n", status);
+        goto cleanupCrHash;
+    }
+    status = BCryptCreateHash(hHashAlg, &hHash, NULL, 0, NULL, 0, 0);
+    if (!NT_SUCCESS(status)) {
+        printf("Error3 0x%x \n", status);
+        goto cleanupCrHash;
+    }
+
+    status = BCryptHashData(hHash, (PBYTE)msg, sizeof(msg), 0);
+    if (!NT_SUCCESS(status)) {
+        printf("Error4 0x%x \n", status);
+        goto cleanupCrHash;
+    }
+
+
+    status = BCryptFinishHash(hHash, pbHash, cbDigest, 0);
+    if (!NT_SUCCESS(status)) {
+        printf("Error5 0x%x \n", status);
+        goto cleanupCrHash;
+    }
+
+cleanupCrHash:
+    if (hHashAlg != NULL)
+    {
+        BCryptCloseAlgorithmProvider(hHashAlg, 0);
+    }
+
+    if (hHash != NULL)
+    {
+        BCryptDestroyHash(hHash);
+    }
+
+    if (pbHash != NULL)
+    {
+        HeapFree(GetProcessHeap(), 0, pbHash);
+    }
+}
+
+
 int main() {
     printf("started \n");
     NTSTATUS status = 0;
     BCRYPT_KEY_HANDLE hkey = 0;
-    BCRYPT_KEY_HANDLE       hTmpKey = 0;
-    BCRYPT_ALG_HANDLE       AlgHandle = 0;
+    BCRYPT_KEY_HANDLE       hVerKey = 0;
+    BCRYPT_ALG_HANDLE       hAlg = 0;
 
-    BCRYPT_ALG_HANDLE       HashAlgHandle = 0;
-    BCRYPT_HASH_HANDLE      HashHandle = 0;
+    BCRYPT_ALG_HANDLE       hHashAlg = 0;
+    BCRYPT_HASH_HANDLE      hHash = 0;
 
 
-    DWORD                   HashDigestLength = 0;
+    DWORD                   cbDigest = 0;
 
-    DWORD                   ResultLength = 0;
+    DWORD                   cbResult = 0;
 
-    PBYTE                   pbHash = NULL,
-        pbHashObject = (PBYTE)HeapAlloc(GetProcessHeap(), 0, HashDigestLength);
-    pbHash = (PBYTE)HeapAlloc(GetProcessHeap(), 0, HashDigestLength);
+    PBYTE                   pbHash = NULL;
+
+    pbHash = (PBYTE)HeapAlloc(GetProcessHeap(), 0, cbDigest);
 
     PBYTE cbSignature = NULL;
 
@@ -38,73 +89,72 @@ int main() {
 
     int naming = 8;
 
-    const wchar_t aaaaaaaa[] = BCRYPT_ECC_CURVE_SECP256R1;
+    const wchar_t pszCurveName[] = BCRYPT_ECC_CURVE_SECP256R1;
+
+    const wchar_t pszAlgName[] = BCRYPT_ECDSA_P256_ALGORITHM;
+
+    //const wchar_t pszAlgNameList[sizeof(pszAlgName)][2] = { pszAlgName, BCRYPT_ECC_CURVE_NAME };
+    BOOL bUseNamedCurveInterface;
+    BOOL bSetCurveNameAsKeyProperty;
+    status = BCryptOpenAlgorithmProvider(
+        &hAlg,
+        pszAlgName,
+        NULL,
+        0);
+    if (!NT_SUCCESS(status))
+    {
+        printf("Error1 key 0x%x \n", status);
+        goto cleanup;
+    }
+
+    //status = BCryptOpenAlgorithmProvider(
+    //    &hAlg,
+    //    pszCurveName,
+    //    NULL,
+    //    0);
+    //if (!NT_SUCCESS(status))
+    //{
+    //    printf("Error openning algorithm provider 0x%x \n", status);
+    //    goto cleanup;
+    //}
 
     switch (naming)
     {
     case 1:
-        status = BCryptOpenAlgorithmProvider(
-            &AlgHandle,
-            BCRYPT_ECDSA_ALGORITHM,
-            NULL,
-            0);
-        if (!NT_SUCCESS(status))
-        {
-            printf("Error1 key 0x%x \n", status);
-            goto cleanup;
-        }
-        status = BCryptSetProperty(AlgHandle, BCRYPT_ECC_CURVE_NAME, (PUCHAR)aaaaaaaa, (wcslen(aaaaaaaa) + 1) * sizeof(wchar_t), 0);
+        bUseNamedCurveInterface = TRUE;
+        status = BCryptSetProperty(hAlg, BCRYPT_ECC_CURVE_NAME, (PUCHAR)pszCurveName, (wcslen(pszCurveName) + 1) * sizeof(wchar_t), 0);
         if (!NT_SUCCESS(status)) {
             printf("Error2 key set property 0x%x \n", status);
             goto cleanup;
         }
-        status = BCryptGenerateKeyPair(AlgHandle, &hkey, 256, 0);
+        status = BCryptGenerateKeyPair(hAlg, &hkey, 256, 0);
         if (!NT_SUCCESS(status)) {
             printf("Error2 key 0x%x \n", status);
             goto cleanup;
         }
         break;
     case 2:
-        status = BCryptOpenAlgorithmProvider(
-            &AlgHandle,
-            BCRYPT_ECDSA_ALGORITHM,
-            NULL,
-            0);
-        if (!NT_SUCCESS(status))
-        {
-            printf("Error1 key 0x%x \n", status);
-            goto cleanup;
-        }
-
-
-
-        status = BCryptGenerateKeyPair(AlgHandle, &hkey, 0, 0);
+        bUseNamedCurveInterface = TRUE;
+        bSetCurveNameAsKeyProperty = TRUE;
+        status = BCryptGenerateKeyPair(hAlg, &hkey, 0, 0);
         if (!NT_SUCCESS(status)) {
             printf("Error2 key 0x%x \n", status);
             goto cleanup;
         }
 
-        status = BCryptSetProperty(hkey, BCRYPT_ECC_CURVE_NAME, (PUCHAR)aaaaaaaa, (wcslen(aaaaaaaa) + 1) * sizeof(wchar_t), 0);
+        status = BCryptSetProperty(hkey, BCRYPT_ECC_CURVE_NAME, (PUCHAR)pszCurveName, (wcslen(pszCurveName) + 1) * sizeof(wchar_t), 0);
         if (!NT_SUCCESS(status)) {
             printf("Error2 key set property 0x%x \n", status);
             goto cleanup;
         }
-
-
-
         break;
     default:
-        status = BCryptOpenAlgorithmProvider(
-            &AlgHandle,
-            BCRYPT_ECDSA_P256_ALGORITHM,
-            NULL,
-            0);
         if (!NT_SUCCESS(status))
         {
             printf("Error1 key 0x%x \n", status);
             goto cleanup;
         }
-        status = BCryptGenerateKeyPair(AlgHandle, &hkey, 256, 0);
+        status = BCryptGenerateKeyPair(hAlg, &hkey, 256, 0);
         if (!NT_SUCCESS(status)) {
             printf("Error2 key 0x%x \n", status);
             goto cleanup;
@@ -120,48 +170,50 @@ int main() {
 
 
 
-    status = BCryptOpenAlgorithmProvider(&HashAlgHandle, BCRYPT_SHA1_ALGORITHM, NULL, 0);
-    if (!NT_SUCCESS(status)) {
-        printf("Error1 0x%x \n", status);
-        goto cleanup;
-    }
-    status = BCryptGetProperty(HashAlgHandle, BCRYPT_HASH_LENGTH, (PBYTE)&HashDigestLength, sizeof(HashDigestLength), &ResultLength, 0);
-    if (!NT_SUCCESS(status)) {
-        printf("Error2 0x%x \n", status);
-        goto cleanup;
-    }
-    status = BCryptCreateHash(HashAlgHandle, &HashHandle, NULL, 0, NULL, 0, 0);
-    if (!NT_SUCCESS(status)) {
-        printf("Error3 0x%x \n", status);
-        goto cleanup;
-    }
+
+    //status = BCryptOpenAlgorithmProvider(&hHashAlg, BCRYPT_SHA1_ALGORITHM, NULL, 0);
+    //if (!NT_SUCCESS(status)) {
+    //    printf("Error1 0x%x \n", status);
+    //    goto cleanup;
+    //}
+    //status = BCryptGetProperty(hHashAlg, BCRYPT_HASH_LENGTH, (PBYTE)&cbDigest, sizeof(cbDigest), &cbResult, 0);
+    //if (!NT_SUCCESS(status)) {
+    //    printf("Error2 0x%x \n", status);
+    //    goto cleanup;
+    //}
+    //status = BCryptCreateHash(hHashAlg, &hHash, NULL, 0, NULL, 0, 0);
+    //if (!NT_SUCCESS(status)) {
+    //    printf("Error3 0x%x \n", status);
+    //    goto cleanup;
+    //}
     static const  BYTE rgbMsg[] =
     {
         0x04, 0x87, 0xec, 0x66, 0xa8, 0xbf, 0x17, 0xa6,
         0xe3, 0x62, 0x6f, 0x1a, 0x55, 0xe2, 0xaf, 0x5e,
         0xbc, 0x54, 0xa4, 0xdc, 0x68, 0x19, 0x3e, 0x94,
     };
-    status = BCryptHashData(HashHandle, (PBYTE)rgbMsg, sizeof(rgbMsg), 0);
-    if (!NT_SUCCESS(status)) {
-        printf("Error4 0x%x \n", status);
-        goto cleanup;
-    }
+    //status = BCryptHashData(hHash, (PBYTE)rgbMsg, sizeof(rgbMsg), 0);
+    //if (!NT_SUCCESS(status)) {
+    //    printf("Error4 0x%x \n", status);
+    //    goto cleanup;
+    //}
 
 
-    status = BCryptFinishHash(HashHandle, pbHash, HashDigestLength, 0);
-    if (!NT_SUCCESS(status)) {
-        printf("Error5 0x%x \n", status);
-        goto cleanup;
-    }
+    //status = BCryptFinishHash(hHash, pbHash, cbDigest, 0);
+    //if (!NT_SUCCESS(status)) {
+    //    printf("Error5 0x%x \n", status);
+    //    goto cleanup;
+    //}
 
+    CrHash(status, hHashAlg, hHash, &cbDigest, cbResult, rgbMsg, pbHash);
 
-    status = BCryptSignHash(hkey, NULL, pbHash, HashDigestLength, NULL, 0, &cbSignature, 0);
+    status = BCryptSignHash(hkey, NULL, pbHash, cbDigest, NULL, 0, &cbSignature, 0);
     if (!NT_SUCCESS(status)) {
         printf("Error6 0x%x \n", status);
         goto cleanup;
     }
     PBYTE pbSignature = (PBYTE)HeapAlloc(GetProcessHeap(), 0, cbSignature);
-    status = BCryptSignHash(hkey, NULL, pbHash, HashDigestLength, pbSignature, cbSignature, &cbSignature, 0);
+    status = BCryptSignHash(hkey, NULL, pbHash, cbDigest, pbSignature, cbSignature, &cbSignature, 0);
     if (!NT_SUCCESS(status)) {
         printf("Error7 0x%x \n", status);
         goto cleanup;
@@ -182,7 +234,7 @@ int main() {
         goto cleanup;
     }
 
-    status = BCryptExportKey(hkey, NULL, BCRYPT_ECCPUBLIC_BLOB, pbBlob, cbBlob, &ResultLength, 0);
+    status = BCryptExportKey(hkey, NULL, BCRYPT_ECCPUBLIC_BLOB, pbBlob, cbBlob, &cbResult, 0);
     if (!NT_SUCCESS(status))
     {
         printf("Error7 ex 2 0x%x \n", status);
@@ -191,7 +243,7 @@ int main() {
 
 
 
-    status = BCryptImportKeyPair(AlgHandle, NULL, BCRYPT_ECCPUBLIC_BLOB, &hkey, pbBlob, cbBlob, 0);
+    status = BCryptImportKeyPair(hAlg, NULL, BCRYPT_ECCPUBLIC_BLOB, &hVerKey, pbBlob, cbBlob, 0);
     if (!NT_SUCCESS(status))
     {
         printf("Error7 ip 0x%x \n", status);
@@ -199,33 +251,31 @@ int main() {
     }
 
     //сделать verify key и оба освободить в коцне
-    status = BCryptVerifySignature(hkey, NULL, pbHash, HashDigestLength, pbSignature, (ULONG)cbSignature, 0);
+    status = BCryptVerifySignature(hVerKey, NULL, pbHash, cbDigest, pbSignature, (ULONG)cbSignature, 0);
     if (!NT_SUCCESS(status)) {
         printf("Error8 ver 0x%x \n", status);
         goto cleanup;
     }
     printf("Verified \n");
 cleanup:
-    /*       if (hkey != NULL) {
-               BcryptDestroyKey(hkey);
-           }*/
-    if (AlgHandle != NULL) {
-        BCryptCloseAlgorithmProvider(AlgHandle, 0);
+    if (hkey != NULL) {
+        BCryptDestroyKey(hkey);
+    }
+    if (hkey != NULL) {
+        BCryptDestroyKey(hVerKey);
+    }
+    if (hAlg != NULL) {
+        BCryptCloseAlgorithmProvider(hAlg, 0);
     }
 
-    if (HashAlgHandle != NULL)
+    if (hHashAlg != NULL)
     {
-        BCryptCloseAlgorithmProvider(HashAlgHandle, 0);
+        BCryptCloseAlgorithmProvider(hHashAlg, 0);
     }
 
-    if (HashHandle != NULL)
+    if (hHash != NULL)
     {
-        BCryptDestroyHash(HashHandle);
-    }
-
-    if (pbHashObject != NULL)
-    {
-        HeapFree(GetProcessHeap(), 0, pbHashObject);
+        BCryptDestroyHash(hHash);
     }
 
     if (pbHash != NULL)
@@ -240,4 +290,3 @@ cleanup:
 
     return 1;
 }
-
